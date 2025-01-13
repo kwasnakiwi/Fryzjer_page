bc0beef3cc1dfb0da1fafe8f29406c45232cec12
kwasnakiwi <jakzab199@gmail.com>
2025-01-13 20:58:39 +0100

    sigma

--- src/sites/Porfolio.jsx
+++ src/sites/Porfolio.jsx
@@ -1,13 +1,13 @@
 import './../styles/Portfolio.css'
-import zdj1 from './../assets/images/Zdjęcie 1.png';
-import zdj2 from './../assets/images/Zdjęcie 2.png';
-import zdj3 from './../assets/images/Zdjęcie 3.png';
-import zdj4 from './../assets/images/Zdjęcie 4.png';
-import zdj5 from './../assets/images/Zdjęcie 5.png';
-import zdj6 from './../assets/images/Zdjęcie 6.png';
-import zdj7 from './../assets/images/Zdjęcie 7.png';
-import zdj8 from './../assets/images/Zdjęcie 8.png';
-import zdj9 from './../assets/images/Zdjęcie 9.png';
+import zdj1 from './../assets/images/portfolio1.png';
+import zdj2 from './../assets/images/portfolio2.png';
+import zdj3 from './../assets/images/portfolio3.png';
+import zdj4 from './../assets/images/portfolio4.png';
+import zdj5 from './../assets/images/portfolio5.png';
+import zdj6 from './../assets/images/portfolio6.png';
+import zdj7 from './../assets/images/portfolio7.png';
+import zdj8 from './../assets/images/portfolio8.png';
+import zdj9 from './../assets/images/portfolio9.png';
 
 function Portfolio() {
   return(
@@ -22,8 +22,8 @@ function Portfolio() {
         <main className='main-box'>
           <div className='images-box'>
             <div className='images'>
-              <img src={zdj1}/>
-              <img className='center-col' src={zdj2}/>
+              <img src={zdj2}/>
+              <img className='center-col' src={zdj1}/>
               <img  src={zdj3}/>
               <img src={zdj4}/>
               <img className='center-col' src={zdj5}/>
